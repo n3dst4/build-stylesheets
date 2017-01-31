458d3624b9b26b0a00701168a9baa488ebb9a49f
Neil de Carteret <git@lumphammer.com>
2017-01-31 11:49:09 +0000

    H5C-435 use less-plugin-npm-inport

--- src/build-stylesheets.js
+++ src/build-stylesheets.js
@@ -5,13 +5,19 @@ import multipipe from "multipipe"
 import plumber from "gulp-plumber"
 import rework from 'gulp-rework';
 import reworkAssets from "rework-assets";
+import NpmImportPlugin from "less-plugin-npm-import";
 
 export default function (assetOptions, production = false) {
   return multipipe(
     // plumber to catch errors
     plumber(),
     // run everything through LESS, converting URLs to be relative to output
-    less({relativeUrls: true}),
+    less({
+      relativeUrls: true,
+      plugins: [
+        new NpmImportPlugin()
+      ]
+    }),
     // use "rework" and "rework-assets" to copy fonts, images etc
     rework(reworkAssets(assetOptions)),
     // minify if needed
